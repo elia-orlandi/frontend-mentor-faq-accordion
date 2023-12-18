0b6a68a3659b0d3493f4a877bef16835ab5c40df
elia.orlandi <elia.orlandi@quix,.it>
2023-12-19 00:05:54 +0100

    feat: structure HTML + little CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['WorkSans', 'sans-serif'],
+      },
+      colors: {
+        'fuchsia-pink': {
+          '50': '#fbf5fe',
+          '100': '#f6eafd',
+          '200': '#edd3fb',
+          '300': '#e2b1f6',
+          '400': '#d283ef',
+          '500': '#b139de',
+          '600': '#a133c6',
+          '700': '#8727a4',
+          '800': '#702286',
+          '900': '#5f216e',
+          '950': '#3c0a48',
+        }
       }
-    },
+    }
   },
   plugins: [],
 }
